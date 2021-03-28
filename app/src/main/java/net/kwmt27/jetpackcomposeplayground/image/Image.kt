37e028a88c574ec0bba3dbb0543857903686d91c
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-28 18:49:59 +0900

    画像を円形にするサンプル追加

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/image/Image.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/image/Image.kt
@@ -1,6 +1,11 @@
 package net.kwmt27.jetpackcomposeplayground.image
 
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.coil.CoilImage
 
@@ -17,4 +22,25 @@ fun NetworkImageSample(
 @Composable
 fun PreviewNetworkImageSample() {
     NetworkImageSample()
+}
+
+@Composable
+fun CircleImageSample() {
+    Surface(
+        shape = CircleShape,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+    ) {
+        CoilImage(
+            data = "https://picsum.photos/300/300",
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCircleImageSample() {
+    CircleImageSample()
 }
