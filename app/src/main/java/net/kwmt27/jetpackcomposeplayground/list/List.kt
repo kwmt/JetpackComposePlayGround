6a093335853f99ddb94f7a7ece1fa13e63b0a85b
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-28 12:16:46 +0900

    add sticky header sample

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/list/List.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/list/List.kt
@@ -1,13 +1,16 @@
 package net.kwmt27.jetpackcomposeplayground.list
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -48,9 +51,17 @@ fun ListItem(index: Int = 0) {
     }
 }
 
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun PreviewSampleList() {
+    JetpackComposePlayGroundTheme {
+        SampleVerticalList()
+    }
+}
+
 @Composable
 fun SampleHorizontalList() {
-    LazyRow{
+    LazyRow {
         items(5) { index ->
             ListHorizontalItem(index)
         }
@@ -78,8 +89,76 @@ fun ListHorizontalItem(index: Int = 0) {
 
 @Preview(widthDp = 360, heightDp = 640)
 @Composable
-fun PreviewSampleList() {
+fun PreviewSampleHorizontalList() {
     JetpackComposePlayGroundTheme {
-        SampleVerticalList()
+        SampleHorizontalList()
+    }
+}
+
+private data class Contact(val firstName: String)
+
+private val contacts =
+    listOf<Contact>(
+        Contact("AAA"),
+        Contact("ABB"),
+        Contact("Bbb"),
+        Contact("Bbb2"),
+        Contact("Bbb3"),
+        Contact("Bbb3"),
+        Contact("Bbb4"),
+        Contact("Ccc"),
+        Contact("Ccc2"),
+        Contact("Ccc3"),
+        Contact("Ccc4"),
+        Contact("Ccc5"),
+    )
+private val grouped = contacts.groupBy { it.firstName[0] }
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun StickyListSample() {
+    LazyColumn {
+        grouped.forEach { (initial, contactsForInitial) ->
+            stickyHeader {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Gray)
+                        .padding(horizontal = 8.dp),
+                    text = initial.toString()
+                )
+            }
+
+            items(contactsForInitial) { contact ->
+                ListItem2(contact.firstName)
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem2(text: String = "") {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .background(Color.Blue)
+                .fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "$text",
+                style = TextStyle(color = Color.White)
+            )
+        }
+        Divider()
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun PreviewStickyListSample() {
+    JetpackComposePlayGroundTheme {
+        StickyListSample()
     }
 }
