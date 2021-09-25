d85b37538fb3c91d77bd87c34e0773ca0d6b59b3
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-09-25 13:40:02 +0900

    expandable card sample

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/NavHost.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/NavHost.kt
@@ -18,11 +18,13 @@ import net.kwmt27.jetpackcomposeplayground.Destinations.MAIN
 import net.kwmt27.jetpackcomposeplayground.Destinations.SAMPLES_ANIMATION
 import net.kwmt27.jetpackcomposeplayground.Destinations.SAMPLES_BOX
 import net.kwmt27.jetpackcomposeplayground.Destinations.SAMPLES_CIRCLE_IMAGE
+import net.kwmt27.jetpackcomposeplayground.Destinations.SAMPLES_EXPANDABLE
 import net.kwmt27.jetpackcomposeplayground.Destinations.SAMPLES_VERTICAL_LIST
 import net.kwmt27.jetpackcomposeplayground.animation.AnimateAsStateDemo
 import net.kwmt27.jetpackcomposeplayground.box.BoxSample
 import net.kwmt27.jetpackcomposeplayground.image.CircleImageSample
 import net.kwmt27.jetpackcomposeplayground.list.SampleVerticalList
+import net.kwmt27.jetpackcomposeplayground.state.ExpandableCardSample
 
 object Destinations {
     private const val SAMPLES = "samples"
@@ -30,11 +32,13 @@ object Destinations {
     private const val VERTICAL_LIST = "vertical-list"
     private const val ANIMATION = "animation"
     private const val BOX = "box"
+    private const val EXPANDABLE = "expandable"
     const val MAIN = "main"
     const val SAMPLES_CIRCLE_IMAGE = "/$SAMPLES/$CIRCLE_IMAGE"
     const val SAMPLES_VERTICAL_LIST = "/$SAMPLES/$VERTICAL_LIST"
     const val SAMPLES_ANIMATION = "/$SAMPLES/$ANIMATION"
     const val SAMPLES_BOX = "/$SAMPLES/$BOX"
+    const val SAMPLES_EXPANDABLE = "/$SAMPLES/$EXPANDABLE"
 }
 
 @Composable
@@ -45,6 +49,7 @@ fun NavHost(navController: NavHostController) {
         composable(SAMPLES_VERTICAL_LIST) { SampleVerticalList() }
         composable(SAMPLES_ANIMATION) { AnimateAsStateDemo() }
         composable(SAMPLES_BOX) { BoxSample() }
+        composable(SAMPLES_EXPANDABLE) { ExpandableCardSample() }
     }
 }
 
@@ -56,6 +61,7 @@ fun MainList(navController: NavHostController) {
         mainListItem(label = "Vertical List") { navController.navigate(SAMPLES_VERTICAL_LIST) }
         mainListItem(label = "Animation") { navController.navigate(SAMPLES_ANIMATION) }
         mainListItem(label = "Box") { navController.navigate(SAMPLES_BOX) }
+        mainListItem(label = "Expandable Card") { navController.navigate(SAMPLES_EXPANDABLE) }
         mainListItem(label = "Color") {
             context.startActivity(Intent(Intent.ACTION_VIEW,
                 Uri.parse("https://github.com/kwmt/JetpackComposePlayGround/blob/main/app/src/main/java/net/kwmt27/jetpackcomposeplayground/color/Color.kt")))
