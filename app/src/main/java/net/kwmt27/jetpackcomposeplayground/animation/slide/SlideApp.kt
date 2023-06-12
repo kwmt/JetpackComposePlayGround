e59a1cb767179cc5ae50e130be8c44ec0a706ca2
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-13 02:24:00 +0900

    auto page めくりとタイマー

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/SlideApp.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/SlideApp.kt
@@ -1,8 +1,11 @@
 package net.kwmt27.jetpackcomposeplayground.animation.slide
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.view.WindowManager
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,20 +13,63 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SlideApp() {
-    Scaffold {
-        HorizontalPager(pageCount = slides.size) {
-            SlideContent(slides[it])
+    val activity = LocalContext.current as Activity
+
+    LaunchedEffect(key1 = Unit) {
+        activity.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+    var currentIndex by remember {
+        mutableStateOf(0)
+    }
+    var currentMillSec by remember {
+        mutableStateOf(5000)
+    }
+    if (currentIndex < slides.size) {
+        LaunchedEffect(key1 = currentIndex) {
+            delay(5000)
+            currentIndex++
+            currentMillSec = 5000
+        }
+        LaunchedEffect(key1 = currentMillSec) {
+            delay(1000)
+            currentMillSec -= 1000
         }
     }
+
+    Scaffold {
+        HorizontalPager(pageCount = slides.size) {
+            if (currentIndex < slides.size) {
+                Box {
+                    SlideContent(slides[currentIndex])
+                    TimerView(currentMillSec)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TimerView(currentMillSec: Int) {
+    Text("$currentMillSec")
 }
 
 @Composable
