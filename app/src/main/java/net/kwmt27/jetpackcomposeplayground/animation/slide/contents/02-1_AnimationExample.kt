0685384228a126696d4822d43940103a07669669
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-19 02:04:42 +0900

    1_AnimationExample.ktを追加

---
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/contents/02-1_AnimationExample.kt
@@ -0,0 +1,34 @@
+package net.kwmt27.jetpackcomposeplayground.animation.slide.contents
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.kwmt27.jetpackcomposeplayground.animation.AutoRollingTextSample5
+import net.kwmt27.jetpackcomposeplayground.animation.slide.SlideTypography
+
+@Composable
+fun AnimationExample() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AutoRollingTextSample5()
+        Spacer(modifier = Modifier.size(16.dp))
+        Column {
+            Text("1.テキストが下から上に回転するようなアニメーション", style = SlideTypography.body1)
+            Spacer(modifier = Modifier.size(16.dp))
+            Text("2.グレーのボックスが左から右にかけて消えていくアニメーション", style = SlideTypography.body1)
+            Spacer(modifier = Modifier.size(16.dp))
+            Text("3.右側の青色が赤色に切り替わるアニメーション", style = SlideTypography.body1)
+        }
+    }
+}
