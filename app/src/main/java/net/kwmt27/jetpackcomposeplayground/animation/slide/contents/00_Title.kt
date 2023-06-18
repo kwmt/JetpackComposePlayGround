10a283c9cf96328f10ca9a00a2d6ac818f6e03cf
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-19 01:00:31 +0900

    fix title

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/contents/00_Title.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/contents/00_Title.kt
@@ -13,7 +13,7 @@ fun SlideTitle() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text("Jetpack Composeでのアニメーション実装事例",  style = SlideTypography.h1)
+        Text("LIPSでのJetpack Composeアニメーション実装事例",  style = SlideTypography.h1)
         Text("potatotips #82")
     }
 }
