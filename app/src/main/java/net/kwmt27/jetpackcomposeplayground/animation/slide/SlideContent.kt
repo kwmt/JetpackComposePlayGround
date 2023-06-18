e0f9c79430d86b910ab1c074b9ee75d73f174433
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-18 17:44:36 +0900

    slideに画像を入れるようにした

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/SlideContent.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/SlideContent.kt
@@ -8,7 +8,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import net.kwmt27.jetpackcomposeplayground.R
 
 @Composable
 internal fun SlideContent(slide: Slide) {
@@ -21,5 +26,24 @@ internal fun SlideContent(slide: Slide) {
     ) {
         Text(text = slide.title)
         Text(text = slide.content)
+        SlideImage(R.drawable.animation_flowchart)
     }
+}
+
+@Composable
+internal fun SlideImage(
+    data: Any?,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Fit,
+) {
+    AsyncImage(
+        model = ImageRequest
+            .Builder(LocalContext.current)
+            .data(data)
+            .crossfade(true)
+            .build(),
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale,
+    )
 }
