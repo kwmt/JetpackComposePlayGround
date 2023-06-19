3e00a7a8c1358c66d4b577c4c07175f402f88456
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-20 01:55:19 +0900

    update 02-3

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/contents/02-3_AnimationExample.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/contents/02-3_AnimationExample.kt
@@ -9,14 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import net.kwmt27.jetpackcomposeplayground.animation.AutoRollingTextSample2
 import net.kwmt27.jetpackcomposeplayground.animation.slide.SlidePreview
@@ -50,6 +47,9 @@ fun AnimationExample3() {
                     )
                 }
             )
+            Spacer(modifier = Modifier.size(16.dp))
+            Text("テキストが下から上に回転するようなアニメーションの完成", style = SlideTypography.body1)
+
             Spacer(modifier = Modifier.size(16.dp))
             Text(
                 text = "実は公式ドキュメント(https://developer.android.com/jetpack/compose/animation?hl=ja#animatedcontent)にそのまま載っているので参考にしてください。",
