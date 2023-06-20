3340418d52a349df46e281c4d9ae7eb7dabd8908
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-20 09:36:40 +0900

    02-3-1

---
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/contents/02-3-1_AnimationExample.kt
@@ -0,0 +1,40 @@
+package net.kwmt27.jetpackcomposeplayground.animation.slide.contents
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.kwmt27.jetpackcomposeplayground.animation.AutoRollingTextSample4
+import net.kwmt27.jetpackcomposeplayground.animation.slide.SlidePreview
+import net.kwmt27.jetpackcomposeplayground.animation.slide.SlideTypography
+import net.kwmt27.jetpackcomposeplayground.animation.slide.components.Code
+
+@Composable
+fun AnimationExample3_1() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AutoRollingTextSample4()
+        Spacer(modifier = Modifier.size(16.dp))
+        Column {
+            Text("isVisibleフラグで色を切り替えてるだけでアニメーションなし", style = SlideTypography.body1)
+            Spacer(modifier = Modifier.size(16.dp))
+            Code(path = "02-3-1_AnimateExample.html")
+        }
+    }
+}
+
+@SlidePreview
+@Composable
+private fun PreviewAnimationExample() {
+    AnimationExample3_1()
+}
