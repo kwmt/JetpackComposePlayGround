e9851805a1b04fb0823aa6f584f5f2c45106e3b0
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-06-20 10:28:55 +0900

    03-1

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/Slide.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/animation/slide/Slide.kt
@@ -9,6 +9,7 @@ import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.AnimationExa
 import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.AnimationExample2_3
 import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.AnimationExample3_1
 import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.AnimationExample3_2
+import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.DetailsAndPoints3_1
 import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.Intro
 import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.Intro2
 import net.kwmt27.jetpackcomposeplayground.animation.slide.contents.Intro3
@@ -32,4 +33,5 @@ internal val slides = listOf(
     Slide("例2: 緑のボックスが左から右にかけて消えていくアニメーション") { AnimationExample2_3() },
     Slide("例3: 右の円が青から赤にクロスフェードするアニメーション") { AnimationExample3_1() },
     Slide("例3: 右の円が青から赤にクロスフェードするアニメーション") { AnimationExample3_2() },
+    Slide("他のアニメーションAPIは？") { DetailsAndPoints3_1() },
 )
