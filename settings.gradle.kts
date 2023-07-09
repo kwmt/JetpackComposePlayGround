16969e5709576d4a67fd19706a269bd98e8e4be5
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-07-09 11:40:12 +0900

    pluginmanager in settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,16 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
     }
