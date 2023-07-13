abbcf433682965cfa2cfbef2cc9ba056a7c06711
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2023-07-14 01:15:33 +0900

    fix

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/slide/contents/shibuyaapk43/05_1_SlideAppTips.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/slide/contents/shibuyaapk43/05_1_SlideAppTips.kt
@@ -1,30 +1,13 @@
 package net.kwmt27.jetpackcomposeplayground.slide.contents.shibuyaapk43
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import net.kwmt27.jetpackcomposeplayground.R
 import net.kwmt27.jetpackcomposeplayground.slide.SlidePreview
-import net.kwmt27.jetpackcomposeplayground.slide.SlideTypography
 import net.kwmt27.jetpackcomposeplayground.slide.components.Code
 import net.kwmt27.jetpackcomposeplayground.slide.components.SlideBase
 import net.kwmt27.jetpackcomposeplayground.slide.components.SlideImage
@@ -45,6 +28,6 @@ internal fun SlideAppTips1() {
 
 @SlidePreview
 @Composable
-private fun PreviewPros() {
+private fun PreviewSlideAppTips() {
     SlideAppTips1()
 }
