d5da840267c93d17491c6c366d3029e60635753b
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2023-07-13 22:13:28 +0900

    update pros

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/slide/contents/shibuyaapk43/03_2_Pros.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/slide/contents/shibuyaapk43/03_2_Pros.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import net.kwmt27.jetpackcomposeplayground.animation.AutoRollingTextSample
+import net.kwmt27.jetpackcomposeplayground.chart.PieChartScreen
 import net.kwmt27.jetpackcomposeplayground.slide.SlidePreview
 import net.kwmt27.jetpackcomposeplayground.slide.SlideTypography
 import net.kwmt27.jetpackcomposeplayground.slide.components.Code
@@ -22,16 +23,33 @@ internal fun Pros2() {
     ) {
         Row {
             Column {
+                Spacer(modifier = Modifier.size(16.dp))
                 Text(
-                    text = "Keynote",
-                    style = SlideTypography.h3
+                    text = "たとえばチャートってどうやって書くんだろう？",
+                    style = SlideTypography.body1
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 Text(
-                    text = "コード触りたいのに keynoteの使い方などをググりながら作ることになるため、keynoteの使い方を知りたいんじゃない、Androidアプリの知識を得たいんだ。keynoteの使い方を調べてる暇があるならAndroidのコードを書きたい！⇛それができる",
+                    text = "「Keynote チャート 使い方」で検索するのではなく",
+                    style = SlideTypography.body1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(
+                    text = "「Compose chart」で検索できる",
+                    style = SlideTypography.body1
+                )
+                Spacer(modifier = Modifier.size(64.dp))
+                Text(
+                    text = "つまり",
+                    style = SlideTypography.body1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(
+                    text = "スライドを作りながら成長ができる",
                     style = SlideTypography.body1
                 )
             }
+            PieChartScreen()
         }
     }
 }
