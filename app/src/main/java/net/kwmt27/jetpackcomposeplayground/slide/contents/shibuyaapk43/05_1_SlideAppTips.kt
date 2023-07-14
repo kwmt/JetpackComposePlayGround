fcca2dfab696eb938645473e04e771dc5a4aba25
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2023-07-14 09:11:33 +0900

    fix lint

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/slide/contents/shibuyaapk43/05_1_SlideAppTips.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/slide/contents/shibuyaapk43/05_1_SlideAppTips.kt
@@ -1,16 +1,10 @@
 package net.kwmt27.jetpackcomposeplayground.slide.contents.shibuyaapk43
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import net.kwmt27.jetpackcomposeplayground.R
 import net.kwmt27.jetpackcomposeplayground.slide.SlidePreview
 import net.kwmt27.jetpackcomposeplayground.slide.components.Code
 import net.kwmt27.jetpackcomposeplayground.slide.components.SlideBase
-import net.kwmt27.jetpackcomposeplayground.slide.components.SlideImage
 
 @Composable
 internal fun SlideAppTips1() {
