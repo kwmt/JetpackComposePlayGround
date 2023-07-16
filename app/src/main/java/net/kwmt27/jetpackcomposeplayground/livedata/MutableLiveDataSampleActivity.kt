2abef387c08700ad837a9dbea165bde6e792cf0d
Yasutaka Kawamoto <kwmt27@gmail.com>
2023-07-17 00:21:39 +0900

    mutable livedata initial確認

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/livedata/MutableLiveDataSampleActivity.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/livedata/MutableLiveDataSampleActivity.kt
@@ -1,15 +1,30 @@
 package net.kwmt27.jetpackcomposeplayground.livedata
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
 import net.kwmt27.jetpackcomposeplayground.R
 
-class MutableLiveDataSampleActivity : AppCompatActivity() {
+class MutableLiveDataSampleActivity :
+    AppCompatActivity(R.layout.activity_mutable_live_data_sample) {
+    private val viewModel: SampleMutableLiveDataViewModel by viewModels()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mutable_live_data_sample)
+        val textView = findViewById<TextView>(R.id.textView)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val text = "onCreate"
+        viewModel.mutableLiveDataNoConstruct.observe(this) {
+            textView.text = "$text\nMutableLiveData(no construct)"
+        }
+        viewModel.mutableLiveDataConstruct.observe(this) {
+            textView2.text = "$text\nMutableLiveData(デフォルト引数あり)\n$it"
+        }
     }
 
     companion object {
--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/livedata/MutableLiveDataViewModel.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/livedata/MutableLiveDataViewModel.kt
@@ -4,11 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SampleMutableLiveDataViewModel: ViewModel() {
-    private val _text = MutableLiveData<String>()
-    val text: LiveData<String> = _text
-
-    fun setText(text: String) {
-        _text.value = text
-    }
+class SampleMutableLiveDataViewModel : ViewModel() {
+    private val _mutableLiveDataNoConstruct = MutableLiveData<String>()
+    val mutableLiveDataNoConstruct: LiveData<String> = _mutableLiveDataNoConstruct
+    private val _mutableLiveDataConstruct = MutableLiveData("default arg")
+    val mutableLiveDataConstruct: LiveData<String> = _mutableLiveDataConstruct
 }
