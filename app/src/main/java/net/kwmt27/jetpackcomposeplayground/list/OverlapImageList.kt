958e43c47b01ac268128121088fb6c23306fed0c
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2023-11-24 13:43:14 +0900

    fix lint

--- app/src/main/java/net/kwmt27/jetpackcomposeplayground/list/OverlapImageList.kt
+++ app/src/main/java/net/kwmt27/jetpackcomposeplayground/list/OverlapImageList.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
