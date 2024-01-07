2a669c9956de9663fd03467be7cabc2943666346
Yasutaka Kawamoto <kwmt27@gmail.com>
2024-01-07 12:12:10 +0900

    move icon to lagacy

--- legacy/src/main/java/net/kwmt27/jetpackcomposeplayground/legacy/feature/icon/Icon.kt
+++ legacy/src/main/java/net/kwmt27/jetpackcomposeplayground/legacy/feature/icon/Icon.kt
@@ -1,4 +1,4 @@
-package net.kwmt27.jetpackcomposeplayground.icon
+package net.kwmt27.jetpackcomposeplayground.legacy.feature.icon
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
